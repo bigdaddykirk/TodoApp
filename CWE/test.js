a88375c051de715411b39961edb681a9c181c531
Kirk Springs <kspringsjr@gmail.com>
2024-02-16 15:03:51 -0500

    TodoApp

---
+++ CWE/test.js
@@ -0,0 +1,14 @@
+var div1 = document.getElementById('div1');
+
+var unicycle = document.getElementsByClassName('unicycle');
+
+var paragraph = document.getElementsByTagName('p');
+
+var queryunicycle = document.querySelector('#unicycle, #div2');
+
+var text = "<h1>Hello World</h1>";
+
+//div1.innerHTML = text;
+
+//for (i = 0; i < queryAll.length; ++i){queryAll[i].innerHTML = text;}
+
